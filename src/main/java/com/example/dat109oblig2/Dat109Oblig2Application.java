9af37af48fabe9eb5995c559fe85c3631381b908
David Hoang Nguyen <David.H.Nguyen1999@gmail.com>
2025-02-13 12:39:54 +0100

    first commit

---
+++ src/main/java/com/example/dat109oblig2/Dat109Oblig2Application.java
@@ -0,0 +1,13 @@
+package com.example.dat109oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat109Oblig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dat109Oblig2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dat109oblig2/Dat109Oblig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dat109oblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat109Oblig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
