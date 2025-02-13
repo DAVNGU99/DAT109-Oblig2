9791672677bc21c825d7bfa06741846eacf804be
h678119 <marcusrongved@gmail.com>
2025-02-13 15:08:06 +0100

    Prisberegning

--- src/main/java/com/example/dat109oblig2/Utleiegruppe.java
+++ src/main/java/com/example/dat109oblig2/Utleiegruppe.java
@@ -1,5 +1,27 @@
 package com.example.dat109oblig2;
 
+import java.math.BigDecimal;
+
 public enum Utleiegruppe {
-	A, B, C, D
+    A, B, C, D;
+
+    public BigDecimal dagsPris() {
+        BigDecimal pris = null;
+
+        switch (this) {
+            case A:
+                pris = new BigDecimal(500);
+                break;
+            case B:
+                pris = new BigDecimal(800);
+                break;
+            case C:
+                pris = new BigDecimal(1200);
+                break;
+            case D:
+                pris = new BigDecimal(1800);
+                break;
+        }
+        return pris;
+    }
 }
